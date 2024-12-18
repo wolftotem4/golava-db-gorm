6fe0ed245d251c20562ac3b554caabec35ede154
Randolph <pkh_randolph@photons.com.tw>
2024-12-18 17:37:13 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/wolftotem4/golava-db-gorm
+
+go 1.23.4
+
+require (
+	github.com/wolftotem4/golava-core v0.1.0
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/alexedwards/argon2id v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+)
---
+++ gormuserprovider.go
@@ -0,0 +1,89 @@
+package gorm
+
+import (
+	"context"
+	"crypto/subtle"
+	"errors"
+	"strings"
+
+	"github.com/wolftotem4/golava-core/auth"
+	"github.com/wolftotem4/golava-core/hashing"
+	"gorm.io/gorm"
+)
+
+type GormUserProvider struct {
+	DB            *gorm.DB
+	Hasher        hashing.Hasher
+	ConstructUser func() auth.Authenticatable
+}
+
+func (p *GormUserProvider) RetrieveById(ctx context.Context, identifier any) (auth.Authenticatable, error) {
+	user := p.ConstructUser()
+	result := p.DB.First(user, identifier)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, auth.ErrUserNotFound
+	}
+	return user, result.Error
+}
+
+func (p *GormUserProvider) RetrieveByToken(ctx context.Context, identifier any, token string) (auth.Authenticatable, error) {
+	user := p.ConstructUser()
+	result := p.DB.First(user, identifier)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, auth.ErrUserNotFound
+	} else if result.Error != nil {
+		return nil, result.Error
+	}
+
+	if subtle.ConstantTimeCompare([]byte(token), []byte(user.GetRememberToken())) != 1 {
+		return nil, auth.ErrUserNotFound
+	}
+
+	return user, nil
+}
+
+func (p *GormUserProvider) UpdateRememberToken(ctx context.Context, user auth.Authenticatable, token string) error {
+	return p.DB.Model(user).Update(user.GetRememberTokenName(), token).Error
+}
+
+func (p *GormUserProvider) RetrieveByCredentials(ctx context.Context, credentials map[string]any) (auth.Authenticatable, error) {
+	var excludePassword = make(map[string]any)
+	for key, value := range credentials {
+		if !strings.Contains(key, "password") {
+			excludePassword[key] = value
+		}
+	}
+
+	if len(excludePassword) == 0 {
+		return nil, auth.ErrUserNotFound
+	}
+
+	user := p.ConstructUser()
+	result := p.DB.Where(excludePassword).First(user)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, auth.ErrUserNotFound
+	}
+	return user, result.Error
+}
+
+func (p *GormUserProvider) ValidateCredentials(ctx context.Context, user auth.Authenticatable, credentials map[string]any) (bool, error) {
+	password, ok := credentials[user.GetAuthPasswordName()]
+	if !ok {
+		return false, nil
+	}
+
+	return p.Hasher.Check(password.(string), user.GetAuthPassword())
+}
+
+func (p *GormUserProvider) RehashPasswordIfRequired(ctx context.Context, user auth.Authenticatable, credentials map[string]any, force bool) error {
+	if !p.Hasher.NeedsRehash(user.GetAuthPassword()) && !force {
+		return nil
+	}
+
+	hash, err := p.Hasher.Make(credentials[user.GetAuthPasswordName()].(string))
+	if err != nil {
+		return err
+	}
+
+	return p.DB.Model(user).Update(user.GetAuthPasswordName(), hash).Error
+}
